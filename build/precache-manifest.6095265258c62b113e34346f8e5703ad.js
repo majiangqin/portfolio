ffb7714580cfaca25aac5f3671f325880702da64
Jiangqin Ma <jma416@gatech.edu>
2024-07-10 11:49:41 -0700

    Commit latest changes

---
+++ build/precache-manifest.6095265258c62b113e34346f8e5703ad.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "86bd9a57b5776ea119a2",
+    "url": "/portfolio/static/css/main.af7081c1.chunk.css"
+  },
+  {
+    "revision": "86bd9a57b5776ea119a2",
+    "url": "/portfolio/static/js/main.e8baa7c9.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "4c17b40725ae93653807",
+    "url": "/portfolio/static/css/2.ba26f84d.chunk.css"
+  },
+  {
+    "revision": "4c17b40725ae93653807",
+    "url": "/portfolio/static/js/2.9c4b0b25.chunk.js"
+  },
+  {
+    "revision": "23273bc8bc3197e29e433d7cf66185fc",
+    "url": "/portfolio/static/media/sklearn_genetic_opt.23273bc8.png"
+  },
+  {
+    "revision": "9c0f986795edbdc96c99bc8853580310",
+    "url": "/portfolio/static/media/kafkaml.9c0f9867.png"
+  },
+  {
+    "revision": "83da797572eddbc916fe8d7919985da8",
+    "url": "/portfolio/static/media/chatbot.83da7975.png"
+  },
+  {
+    "revision": "701aae00cad3c6947734c72558ba0525",
+    "url": "/portfolio/static/media/lidar.701aae00.png"
+  },
+  {
+    "revision": "1cd39c8024a569df62c60dd460ce088a",
+    "url": "/portfolio/static/media/food.1cd39c80.png"
+  },
+  {
+    "revision": "764e309153619d95a0f3baa509d9097c",
+    "url": "/portfolio/static/media/sdxl.764e3091.png"
+  },
+  {
+    "revision": "f7d742fc14d3d495da2581043372d15a",
+    "url": "/portfolio/static/media/fastapi_sklearn.f7d742fc.png"
+  },
+  {
+    "revision": "fed0850a36ae8cfbe9ceaa1ce970c5cc",
+    "url": "/portfolio/static/media/graph_embeddings.fed0850a.png"
+  },
+  {
+    "revision": "4b632d51a45152af9b70077a3d4955ca",
+    "url": "/portfolio/static/media/portfolio.4b632d51.png"
+  },
+  {
+    "revision": "3ecf382195aa190e5b18a37e0db9ba9a",
+    "url": "/portfolio/index.html"
+  }
+];
