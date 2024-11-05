87a9ad51bd91ea8236c43ce6e944f158e7ec01ea
Jiangqin Ma <jma416@gatech.edu>
2024-11-04 22:56:04 -0800

    revise home page

--- src/assets/configs/homeConfig.js
+++ src/assets/configs/homeConfig.js
@@ -24,7 +24,7 @@ const homeConfig = {
     },
     workTimeline: [
         {
-            id: "work-4",
+            id: "work-7",
             title: "Lead Data Scientist",
             company: "Connected to Culture",
             description: "Implemented Machine Learning models for user engagement and content recommendation. \n" +
@@ -33,9 +33,18 @@ const homeConfig = {
             icon: <FaMobileAlt/>,
             tags: ["ml", "python", "GCP", "llm", "chatbot"]
         },
+        {
+            id: "work-6",
+            title: "AI Data Analyst",
+            company: "Welocalize",
+            description: "Conducted research per project guidelines to ensure data accuracy across maps, news, and audio tasks. Transformed raw data into actionable insights, enhancing content quality and user experience. Verified and categorized data to support reliable, data-driven decisions for a wide audience. " ,
+            date: "Jan 2023-Present",
+            icon: <FaMobileAlt/>,
+            tags: ["ml", "llm", "excel"]
+        },
 
         {
-            id: "work-3",
+            id: "work-5",
             title: "Student Researcher",
             company: "Georgia Tech",
             description: "Master technologies including 3D laser scanning, LiDAR, and GPS. Employ machine learning, computer vision, AI, pattern\n" +
@@ -46,19 +55,16 @@ const homeConfig = {
             tags: ["ml", "deeplearning", "python", "gis", "lidar", "gps"]
         },
         {
-            id: "work-2",
-            title: "AI research intern",
+            id: "work-4",
+            title: "AI researcher",
             company: "Blockonomics",
-            description: "Developed and implemented advanced machine learning models to predict Bitcoin mempool behavior, enhancing transaction\n" +
-                "fee estimation and user experience in the Bitcoin network.\n" +
-                "Created a robust system for real-time mempool trend forecasting, providing valuable insights for cryptocurrency users and\n" +
-                "potentially reducing transaction costs.",
-            date: "Aug 2023- Oct 2023",
+            description: "Built advanced machine learning models to predict Bitcoin mempool behavior, improving transaction fee estimation and user experience. Developed a real-time trend forecasting system, delivering insights to help cryptocurrency users reduce transaction costs.",
+            date: "Aug 2023- Nov 2023",
             icon: <DiCodeigniter/>,
             tags: ["ml", "deeplearning", "python", "blockchain", "bitcoin"]
         },
         {
-            id: "work-1",
+            id: "work-3",
             title: "Data Science Practicum",
             company: "The Home Depots",
             description: "Engineered robust data pipeline to process diverse datasets, including structured product attributes, unstructured text, and\n" +
@@ -69,7 +75,15 @@ const homeConfig = {
             icon: <DiCodeigniter/>,
             tags: ["ml", "deeplearning", "python", "generative ai", "hugging face"]
         },
-
+       {
+            id: "work-2",
+            title: "Financial Analyst",
+            company: "New York Life",
+            description: "Performed financial analysis to assess clients' insurance and investment needs, providing tailored solutions for long-term security. Developed customized financial plans with a focus on life insurance, annuities, and retirement strategies. Presented financial products and offered guidance to help clients make informed decisions for their personal and business goals.",
+            date: "Jan 2022 - ADec 2022",
+            icon: <DiCodeigniter/>,
+            tags: ["ml", "forecasting", "risk assessment", "financial analysis", "SQL"]
+        },
     ]
 }
 
